864363c9e15807383f16272ce57fc8b0e1b8b82e
Fabio Silva <fsilva.developer@gmail.com>
2022-03-14 22:53:10 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/br/com/fabio/curso/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package br.com.fabio.curso.services.exceptions;
+
+// Tratamento de erros
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
