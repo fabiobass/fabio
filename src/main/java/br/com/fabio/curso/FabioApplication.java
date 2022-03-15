43b0f558a5a3f327c7317ffd3060b0bb1f976ecd
Fabio Silva <fsilva.developer@gmail.com>
2022-03-14 23:35:13 -0300

    Estado e cidade

--- src/main/java/br/com/fabio/curso/FabioApplication.java
+++ src/main/java/br/com/fabio/curso/FabioApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.fabio.curso.domain.Categoria;
+import br.com.fabio.curso.domain.Cidade;
+import br.com.fabio.curso.domain.Estado;
 import br.com.fabio.curso.domain.Produto;
 import br.com.fabio.curso.repositories.CategoriaRepository;
+import br.com.fabio.curso.repositories.CidadeRepository;
+import br.com.fabio.curso.repositories.EstadoRepository;
 import br.com.fabio.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class FabioApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FabioApplication.class, args);
@@ -44,6 +54,22 @@ public class FabioApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+		
+		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado2);
+		
+		estado1.getCidades().addAll(Arrays.asList(cidade2));
+		estado2.getCidades().addAll(Arrays.asList(cidade1, cidade3));
+		
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
+		
+		
 	}
 
 }
