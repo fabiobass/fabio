e8c745452a48598d04556c4cab8f4d9d7698f09b
Fabio Silva <fsilva.developer@gmail.com>
2022-03-16 21:54:10 -0300

    Listando todas as Categorias  com DTO

--- src/main/java/br/com/fabio/curso/services/CategoriaService.java
+++ src/main/java/br/com/fabio/curso/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.fabio.curso.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,5 +46,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir "
 					+ "categorias que possui produtos");
 		}
+	}
+	
+	public List <Categoria> findAll() {
+		return repo.findAll();
 	}
 }
