37e87ee147b1d6e66f4615990bc35cbdea30888d
Fabio Silva <fsilva.developer@gmail.com>
2022-03-31 00:30:18 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/fabio/curso/config/DevConfig.java
+++ src/main/java/br/com/fabio/curso/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.fabio.curso.services.DBService;
+import br.com.fabio.curso.services.EmailService;
+import br.com.fabio.curso.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,5 +30,11 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		
+		return new SmtpEmailService();
 	}
 }
