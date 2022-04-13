f68546f2e4fd8ce4043ef97825171f895860490e
Fabio Silva <fsilva.developer@gmail.com>
2022-04-13 00:09:32 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/br/com/fabio/curso/FabioApplication.java
+++ src/main/java/br/com/fabio/curso/FabioApplication.java
@@ -1,12 +1,18 @@
 package br.com.fabio.curso;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.fabio.curso.services.S3Service;
+
 @SpringBootApplication
 public class FabioApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FabioApplication.class, args);
 	}
@@ -14,6 +20,7 @@ public class FabioApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		s3Service.uploadFile("C:\\temp\\fotos\\fabio.jpg");
 	}
 
 }
